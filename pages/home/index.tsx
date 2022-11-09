adac0a33ba4ef08db947a9b1f1ffea9ea1851c3d
Edward Naidoo <securedbread@protonmail.com>
2022-11-09 09:11:59 -0800

    Update index.tsx
    Formatting

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -6,28 +6,28 @@ import Projects from "./Projects";
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
 
       {/* Pages */}
       <section className="p-8 py-40 bg-orange-100">
         <h1 className="font-extrabold text-9xl">Company Name</h1>
         <h3 className="py-5 text-3xl font-extralight">Company Slogan</h3>
       </section>
-      
+
       <section className="p-40 bg-orange-400">
         <h1>About</h1>
       </section>
-        
+
       {/* Shows the company projects worked. */}
-      <Projects 
-        title={"Title"} 
-        img={"Image"} 
-        description={"Description"} 
-        date={"The date"}  
+      <Projects
+        title={"Title"}
+        img={"Image"}
+        description={"Description"}
+        date={"The date"}
       />
 
       <h1>Community</h1>
-      <Footer/>
+      <Footer />
     </>
   )
 }
